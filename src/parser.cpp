b1b4a70bc54af50ee64c40167cf45e9fcd3ec7ee
murrayinglis <murrayinglis101@gmail.com>
2024-07-29 21:34:11 +0200

    propogate settings logic

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,7 @@
 #include "parser.hpp"
 #include <uhd/types/device_addr.hpp>
 #include <uhd/device.hpp>
+#include <string>
 
 Parser::Parser()
 {
@@ -22,10 +23,26 @@ Parser::Parser(const std::string& xmlFile)
 
 void Parser::parse()
 {
+    pugi::xml_node deviceNode = doc.child("device");
+    if (!deviceNode)
+    {
+        std::cerr << "Device node missing." << std::endl;
+        return;
+    }
+    pugi::xml_node deviceName = deviceNode.child("name");
+    pugi::xml_node deviceID = deviceNode.child("id");
+
     // Identify device
+    std::cout << "Searching for device: " << deviceName.child_value() << ", with ID: " << deviceID.child_value() << std::endl;
     uhd::device_addr_t hint; //an empty hint discovers all devices
-    uhd::device_addrs_t dev_addrs = uhd::device::find(hint);
+    uhd::device_addrs_t dev_addrs = uhd::device::find(hint); // vector of device addresses
     std::cout << "Devices found: " << dev_addrs.size() << std::endl;
+    uhd::device_addr_t desiredDevice((std::string)(deviceName.child_value()));
 
     // Propogate settings to device
+    pugi::xml_node configNode = doc.child("config");
+    pugi::xml_node centreFrequency = configNode.child("centre_frequency");
+    pugi::xml_node sampleRate = configNode.child("sample_rate");
+    pugi::xml_node gain = configNode.child("gain");
+    pugi::xml_node waveformPath = configNode.child("waveform_path");
 }
