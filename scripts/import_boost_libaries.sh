28cbdfa66bf8fdbcfc7984ef370396f13267519c
murrayinglis <murrayinglis101@gmail.com>
2024-10-01 16:46:46 +0200

    package

--- scripts/import_boost_libaries.sh
+++ scripts/import_boost_libaries.sh
@@ -2,6 +2,7 @@
 
 # Directory where Boost libraries are located
 BOOST_LIB_DIR="/home/murray/boost_1_82_0/stage/lib"
+BOOST_LIB_1_74_0_DIR="/usr/lib/x86_64-linux-gnu"
 
 # Destination directory in your package structure
 PACKAGE_LIB_DIR="/home/murray/sdr-config/sdr-config-package/usr/local/lib"
@@ -12,3 +13,7 @@ mkdir -p $PACKAGE_LIB_DIR
 # Find and copy all .so and .a files from Boost library directory
 find $BOOST_LIB_DIR -name "*.so" -exec cp {} $PACKAGE_LIB_DIR \;
 find $BOOST_LIB_DIR -name "*.a" -exec cp {} $PACKAGE_LIB_DIR \;
+
+find $BOOST_LIB_1_74_0_DIR -name "*boost*.so*" -exec cp {} $PACKAGE_LIB_DIR \;
+find $BOOST_LIB_1_74_0_DIR -name "*boost*.a*" -exec cp {} $PACKAGE_LIB_DIR \;
+
